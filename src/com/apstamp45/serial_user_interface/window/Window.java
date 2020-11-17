6e6734dc8e8e84d51a52849b62a19d6112b65604
apstamp45 <origamifreak55@gmail.com>
2020-11-17 14:53:06 -0500

    Made the window.

--- src/com/apstamp45/serial_user_interface/window/Window.java
+++ src/com/apstamp45/serial_user_interface/window/Window.java
@@ -1,6 +1,17 @@
 package com.apstamp45.serial_user_interface.window;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -14,6 +25,46 @@ public class Window extends Application {
     /** Opens a window. */
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        //Top HBox
+        MenuButton port = new MenuButton("Select Port");
+        Button refreshButton = new Button("Refresh");
+        Region spacer1 = new Region();
+        HBox.setHgrow(spacer1, Priority.ALWAYS);
+        Label portLabel = new Label("LABEL");
+        Region spacer2 = new Region();
+        HBox.setHgrow(spacer2, Priority.ALWAYS);
+        MenuButton baudRate = new MenuButton("Select Baud");
+        HBox topHbox = new HBox(port, refreshButton, 
+            spacer1, portLabel, spacer2, baudRate);
+        topHbox.setPadding(new Insets(5));
+        topHbox.setSpacing(5);
+
+        // TextArea
+        TextArea serialIn = new TextArea();
+        VBox.setVgrow(serialIn, Priority.ALWAYS);
+        serialIn.setEditable(false);
+
+        // Bottom HBox
+        TextField serialOut = new TextField();
+        HBox.setHgrow(serialOut, Priority.ALWAYS);
+        Button send = new Button("Send");
+        HBox bottomHBox = new HBox(serialOut, send);
+        bottomHBox.setPadding(new Insets(5));
+        bottomHBox.setSpacing(5);
+
+        // VBox
+        VBox vBox = new VBox(topHbox, serialIn, bottomHBox);
+
+        // Main scene
+        Scene mainScene = new Scene(vBox);
+
+        // Main Stage
+        primaryStage.setScene(mainScene);
+        primaryStage.setWidth(512);
+        primaryStage.setHeight(512);
+        primaryStage.setMinWidth(354);
+        primaryStage.setMinHeight(354);
         primaryStage.setTitle("Serial-User Interface");
         primaryStage.show();
     }
